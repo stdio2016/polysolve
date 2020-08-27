2a72b76c92da9236c8142dd76529cb436e34e9e1
stdio2016 <randomfrank8@gmail.com>
2020-08-27 18:02:05 +0800

    burrtools converter output

--- converter/burr.py
+++ converter/burr.py
@@ -2,6 +2,8 @@ import argparse
 import gzip
 import xml.etree.ElementTree as ET
 import re
+import sys
+import json
 
 arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument('-i', '--input', type=str, help='Polycube file name')
@@ -11,14 +13,41 @@ args = arg_parser.parse_args()
 inp = gzip.open(args.input, 'r')
 root = ET.fromstring(inp.read().decode('1252'))
 
-def parseVoxel(tt):
+fout = sys.stdout
+if args.output:
+    fout = open(args.output, "w")
+
+def convertGrid(grid, sx, sy, sz, voxel):
+    ans = []
+    if grid == 0 or grid == 2:
+        ans = [[[voxel[x + sx*(y + sy*z)] for x in range(sx)]
+                for y in range(sy)] for z in range(sz)]
+    else:
+        pass
+    return ans
+
+def writeGrid(fout, voxel, dim=3, indent=0):
+    if dim <= 1:
+        fout.write(json.dumps(voxel))
+    else:
+        fout.write('[\n')
+        for i,x in enumerate(voxel):
+            fout.write('  ' * (indent+1))
+            writeGrid(fout, x, dim-1, indent+1)
+            if i < len(voxel)-1: fout.write(',')
+            fout.write('\n')
+        fout.write('  ' * indent + ']')
+
+def parseVoxel(tt, grid):
     sx,sy,sz = tt.attrib['x'], tt.attrib['y'], tt.attrib['z']
     sx = int(sx)
     sy = int(sy)
     sz = int(sz)
     print('\tvoxel size: %dx%dx%d'%(sx,sy,sz), end='')
+    name = ''
     if 'name' in tt.attrib:
-        print(' name: %s' % tt.attrib['name'], end='')
+        name = tt.attrib['name']
+        print(' name: %s' % name, end='')
     print('')
     #print('\t'+tt.text)
     cc = re.compile('[_#+][0-9]*')
@@ -35,7 +64,18 @@ def parseVoxel(tt):
             print('')
         curz += zcnt
         print('')
-    return {'x':sx, 'y':sy, 'z':sz, 'voxel':voxel}
+    for i in range(sx*sy*sz):
+        v = voxel[i]
+        if v[0] == '_':
+            voxel[i] = 0
+        elif v[0] == '#':
+            if len(v) == 1: voxel[i] = 1
+            else: voxel[i] = "c" + v[1:]
+        elif v[0] == '+':
+            if len(v) == 2: voxel[i] = 2
+            else: voxel[i] = "v" + v[1:]
+    voxel = convertGrid(grid, sx, sy, sz, voxel)
+    return {'name': name, 'x':sx, 'y':sy, 'z':sz, 'voxel':voxel}
 
 def parseProblem(tt):
     name = ''
@@ -98,13 +138,50 @@ def parseProblem(tt):
         'bitmap': bitmap
     }
 
+def writeProblem(fout, gridType, colors, shapes, problem, comment):
+    fout.write('{\n')
+    if gridType == 0 or gridType == 2:
+        gridname = 'cube'
+    else:
+        raise RuntimeError('Grid type %d is not supported'%gridType)
+    if problem['name']:
+        fout.write('  "description": %s,\n' % json.dumps(problem['name']))
+    fout.write('  "grid": "%s",\n' % gridname)
+    board = problem['result']
+    board = shapes[board]
+    fout.write('  "board": {\n    "tiles": ')
+    writeGrid(fout, board['voxel'], indent=2)
+    fout.write('\n  },\n')
+    if problem['maxHoles'] > -1:
+        fout.write('  "maxHoles": %d,\n', problem['maxHoles'])
+    fout.write('  "shapes": [\n')
+    for i, shape in enumerate(problem['shapes']):
+        voxel = shapes[shape['id']]
+        fout.write('    {\n')
+        if voxel['name']:
+            fout.write('      "name": "%s",\n' % voxel['name'])
+        if shape['max'] != 1 or shape['min'] != 1:
+            fout.write('      "amount": {"min": %d, "max": %d},\n' % (shape['min'],shape['max']))
+        fout.write('      "morphs": [{\n        "tiles": ')
+        writeGrid(fout, voxel['voxel'], indent=4)
+        fout.write('\n      }]\n    }')
+        if i+1 < len(problem['shapes']): fout.write(',')
+        fout.write('\n')
+    if comment:
+        fout.write('  ],\n')
+        fout.write('  "notes": %s\n' % json.dumps(comment))
+    else:
+        fout.write('  ]\n')
+    fout.write('}\n')
 
 def parsePuzzle(tree):
     if tree.tag != 'puzzle':
         raise RuntimeError('root node is not puzzle')
     
     a = tree.find('gridType')
+    gridType = 0
     if a is not None:
+        gridType = int(a.attrib['type'])
         print('Grid type:', a.attrib['type'])
     
     a = tree.find('colors')
@@ -121,7 +198,7 @@ def parsePuzzle(tree):
     if a is not None:
         print('Shapes:')
         for tt in a.findall('voxel'):
-            shapes.append(parseVoxel(tt))
+            shapes.append(parseVoxel(tt, gridType))
     
     a = tree.find('problems')
     problems = []
@@ -136,5 +213,11 @@ def parsePuzzle(tree):
     if a is not None and a.text:
         comment = a.text
         print('Comment:\n' + a.text)
+    
+    for problem in problems:
+        writeProblem(fout, gridType, colors, shapes, problem, comment)
 
 parsePuzzle(root)
+
+if args.output:
+    fout.close()
