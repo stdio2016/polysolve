f048ae0a490434afd3448558463a794fb8b8c01c
stdio2016 <randomfrank8@gmail.com>
2020-09-02 23:56:17 +0800

    no need to use batch to compile

--- clang.bat
+++
@@ -1,5 +0,0 @@
-rem use clang to build
-clang++ -O3 -fopenmp -c -I include -g src/*.cpp
-clang++ --cuda-path="%CUDA_PATH%" --cuda-gpu-arch=sm_30 -O3 -c -I include -g src/*.cu
-clang++ -L "%CUDA_PATH%\lib\x64" -lcudart_static -lcuda -fopenmp -g -o polysolve.exe *.o
-del *.o
